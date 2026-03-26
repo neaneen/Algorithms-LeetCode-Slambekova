4f07439d6c1ada843149c26b113d1230d2e8c095
neaneen <nuraydlambekova@gmail.com>
2026-03-26 10:59:02 +0500

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.45%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def postorderTraversal(self, root):
+      
+        result = []
+        
+        def postorder(node):
+            if not node:
+                return
+            postorder(node.left)      
+            postorder(node.right)     
+            result.append(node.val)   
+        
+        postorder(root)
+        return result
